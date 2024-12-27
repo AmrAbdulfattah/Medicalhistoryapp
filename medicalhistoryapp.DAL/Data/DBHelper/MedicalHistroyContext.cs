a3ef17f74b9f4d2107bb86a4da90f357bfd0c75d
AmrAbdulfattah <amr.abdulfattah2@gmail.com>
2024-12-27 17:03:19 +0200

    create structure

---
+++ medicalhistoryapp.DAL/Data/DBHelper/MedicalHistroyContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medicalhistoryapp.DAL.Data.DBHelper
+{
+    public class MedicalHistroyContext : DbContext
+    {
+
+        public MedicalHistroyContext()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
